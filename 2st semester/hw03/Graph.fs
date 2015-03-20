f39d79194ba3bdd3b693675cc331d8d99df4b2cd
SergeevED <sergeev.ev.dm@gmail.com>
2015-03-20 16:28:51 +0300

    hw03
    hw03

---
+++ 2st semester/hw03/Graph.fs
@@ -0,0 +1,211 @@
+﻿type IGraph<'A when 'A : equality> =
+  interface
+    abstract Print : List<int> -> unit
+    abstract GetSuccByIndex : int -> List<'A>
+    abstract GetPredByIndex : int -> List<'A>
+    abstract GetSuccByValue : 'A -> List<'A>
+    abstract GetPredByValue : 'A -> List<'A>
+  end
+
+type MatrixGraph<'A when 'A : equality>(V : 'A[] , M : bool[,]) =
+  class
+    let size = V.Length
+    let vertices = V 
+    let adjMatrix = M
+    let isMarked = Array.zeroCreate<bool> size
+    do if (size <> Array2D.length1 M) || (size <> Array2D.length2 M) then failwith "Wrong argument"
+    interface IGraph<'A> with   
+      member this.Print(list) = 
+        List.iter (fun x -> if (x < size) then printf "%A ; " vertices.[x] else failwith "Bounds violation") list
+        printf "\n"
+      member this.GetSuccByIndex index = 
+        if (index >= size || index < 0) 
+        then 
+          failwith "Wrong argument" 
+        else
+          for i = 0 to size - 1 do isMarked.[i] <- false
+          let rec f i j list =
+            isMarked.[i] <- true
+            if j >= size 
+              then 
+                list
+              else
+                match adjMatrix.[i,j] , isMarked.[j] with
+                | true , false -> vertices.[j] :: (f j 0 list) @ (f i (j+1) list)
+                | _ , _ -> f i (j+1) list
+          f index 0 []
+      member this.GetPredByIndex index = 
+        if (index >= size || index < 0) 
+        then 
+          failwith "Wrong argument" 
+        else
+          for i = 0 to size - 1 do isMarked.[i] <- false
+          let rec f i j list =
+            isMarked.[i] <- true
+            if j >= size 
+              then 
+                list
+              else
+                match adjMatrix.[j,i] , isMarked.[j] with
+                | true , false -> vertices.[j] :: (f j 0 list) @ (f i (j+1) list)
+                | _ , _ -> f i (j+1) list
+          f index 0 []
+      member this.GetSuccByValue elem =
+        match Array.tryFindIndex ((=)elem) vertices with 
+        | Some x -> (this :> IGraph<'A>).GetSuccByIndex x
+        | None -> []
+      member this.GetPredByValue elem =
+        match Array.tryFindIndex ((=)elem) vertices with 
+        | Some x -> (this :> IGraph<'A>).GetPredByIndex x
+        | None -> []
+  end
+
+type ListGraph<'A when 'A : equality>(V : 'A[] , L : list<int>[]) =
+  class
+    let size = V.Length
+    let vertices = V 
+    let adjList = L
+    let isMarked = Array.zeroCreate<bool> size
+    do if (size <> Array.length L) then failwith "Wrong argument"
+    do for list in L do for x in list do if (x < 0) || (x > size) then failwith "Wrong argument"
+    interface IGraph<'A> with
+      member this.Print(list) = 
+        List.iter (fun x -> if (x < size) then printf "%A ; " vertices.[x] else failwith "Bounds violation") list
+        printf "\n"
+      member this.GetSuccByIndex index = 
+        if (index >= size || index < 0) 
+        then 
+          failwith "Wrong argument" 
+        else
+          for i = 0 to size - 1 do isMarked.[i] <- false
+          let rec f list  =
+            match list with
+            | [] -> []
+            | a :: list -> 
+              if isMarked.[a] 
+                then 
+                  f list 
+                else 
+                  isMarked.[a] <- true
+                  vertices.[a] :: (f adjList.[a]) @ (f list) 
+          isMarked.[index] <- true
+          f adjList.[index]
+      member this.GetPredByIndex index = 
+        if (index >= size || index < 0) 
+          then 
+            failwith "Wrong argument" 
+          else
+            for i = 0 to size - 1 do isMarked.[i] <- false
+            let rec f i j list =
+              if i >= size 
+                then 
+                  []
+                else 
+                  match List.tryFind ((=)j) adjList.[i] with
+                  | Some _ ->
+                    if isMarked.[i] 
+                      then
+                        f (i+1) j list  
+                      else
+                        isMarked.[i] <- true 
+                        vertices.[i] :: (f 0 i []) @ (f (i+1) j list) 
+                  | None -> f (i+1) j list
+            isMarked.[index] <- true  
+            f 0 index []
+      member this.GetSuccByValue elem =
+        match Array.tryFindIndex ((=)elem) vertices with 
+        | Some x -> (this :> IGraph<'A>).GetSuccByIndex x
+        | None -> []
+      member this.GetPredByValue elem =
+        match Array.tryFindIndex ((=)elem) vertices with 
+        | Some x -> (this :> IGraph<'A>).GetPredByIndex x
+        | None -> []
+  end
+
+type ILabeledGraph<'A when 'A : equality> =
+  interface
+    inherit IGraph<'A>
+    abstract GetPath : 'A -> 'A -> list<'A>           //returns path from one vertex to another vertex
+  end
+
+type Computer = {
+  number : int; 
+  os : string;
+  infectedChance : int;
+  mutable isInfected : bool;
+  }
+
+type LAN(V : Computer[] , M : bool[,]) =
+  class
+    inherit MatrixGraph<Computer>(V , M)
+    let size = V.Length
+    let vertices = V 
+    let adjMatrix = M
+    let isMarked = Array.zeroCreate<bool> size
+    member this.TryInfect() =  
+      let random = System.Random()
+      let rec f i j list =
+        if i >= size
+          then
+            list
+          else
+            if j >= size
+              then
+                f (i+1) 0 list
+              else
+                 if (adjMatrix.[i,j] = true) && (random.Next(100) > 100 - vertices.[j].infectedChance) 
+                   then j :: (f i (j+1) list) 
+                   else f i (j+1) list
+      let infected = f 0 0 []
+      List.iter (fun x -> vertices.[x].isInfected <- true) infected 
+    member this.Show() =
+      for i = 0 to size - 1 do 
+        printf "Computer %d : " vertices.[i].number
+        if vertices.[i].isInfected then printf "infected\n" else printf "not infected\n" 
+    end
+
+[<EntryPoint>]
+let main args =
+  printf "Graph: 10 -> 11 -> 12 ; 11 -> 13 -> 12 ; 13 -> 14 (That`s single graph)\n"
+  let vertices = [| 10 ; 11 ; 12 ; 13 ; 14 |]
+  let adjMatrix = Array2D.zeroCreate<bool> 5 5
+  adjMatrix.[0,1] <- true; adjMatrix.[1,2] <- true; adjMatrix.[1,3] <- true; adjMatrix.[3,2] <- true; adjMatrix.[3,4] <- true;
+  let mGraph = new MatrixGraph<int>(vertices, adjMatrix)
+  printf "Graph with adjacency matrix : "
+  (mGraph :> IGraph<int>).Print [ 0 ; 1 ; 2 ; 3 ; 4 ]
+  printf "%A\n" adjMatrix
+  printf "Successors of '11' : "
+  printf "%A\n" ((mGraph :> IGraph<int>).GetSuccByValue 11)
+  printf "Predecessors of '14' : "
+  printf "%A\n" ((mGraph :> IGraph<int>).GetPredByValue 14)
+
+  let adjLists = [| [1] ; [2;3] ; [] ; [2;4] ; [] |]
+  let lGraph = new ListGraph<int>(vertices, adjLists)
+  printf "Graph with adjacency lists : "
+  (lGraph :> IGraph<int>).Print [ 0 ; 1 ; 2 ; 3 ; 4 ]
+  printf "%A\n" adjLists
+  printf "Successors of '11' : "
+  printf "%A\n" ((lGraph :> IGraph<int>).GetSuccByValue 11)
+  printf "Predecessors of '14' : "
+  printf "%A\n" ((lGraph :> IGraph<int>).GetPredByValue 14)
+
+  printf "\n"
+  printf "LAN\n"
+  let lan = [|
+               { number = 1; os = "Windows 98"; infectedChance = 20; isInfected = false} 
+               { number = 2; os = "Linux";      infectedChance = 15; isInfected = false} 
+               { number = 3; os = "FreeBSD";    infectedChance = 10; isInfected = false} 
+               { number = 4; os = "Windows 95"; infectedChance = 30; isInfected = true} 
+               { number = 5; os = "MenuetOS";   infectedChance = 5;  isInfected = false} 
+            |]
+  let m3 = Array2D.zeroCreate<bool> 5 5
+  for i = 0 to 4 do for j = 0 to 4 do Array2D.set m3 i j true
+  let J = new LAN(lan, m3)
+  Array.iter (fun x -> printf "%A\n" x) lan 
+  (J :> IGraph<Computer>).Print([1])
+  for i = 1 to 5 do 
+    printf "Check %d\n" i 
+    J.Show()
+    J.TryInfect()
+    
+  0
---
+++ 2st semester/hw03/List.fs
@@ -0,0 +1,236 @@
+﻿type IList<'A when 'A : equality> =
+  interface
+    abstract InsertHead : 'A -> unit
+    abstract InsertTail : 'A -> unit
+    abstract InsertAt   : 'A -> int -> unit
+    abstract RemoveHead : unit -> unit
+    abstract RemoveTail : unit -> unit
+    abstract RemoveNode   : 'A -> unit
+    abstract TryFind    : ('A -> bool) -> Option<'A>
+    abstract Concat     : IList<'A> -> unit
+  end
+
+type List<'A> = Void | Node of 'A * List<'A>
+
+type AbstractList<'A when 'A : equality>(L : List<'A>) =
+  class
+    let mutable thisList = L
+
+    member this.GetList() = thisList
+
+    member this.PrintList() = 
+      let rec f list =
+        match list with
+        | Void -> ()
+        | Node(a, next) -> 
+          printf "%A " a
+          f next
+      f thisList
+      printf "\n"
+
+    interface IList<'A> with
+
+      member this.InsertHead(elem) = 
+        thisList <- Node(elem, thisList)
+
+      member this.InsertTail(elem) =
+        let rec f list =
+          match list with
+          | Void -> Node(elem, Void)
+          | Node(a, next) -> Node(a, f next)
+        thisList <- f thisList
+
+      member this.InsertAt elem index =   //if index > length of list then inserts element at tail
+        if index < 1 
+          then 
+            failwith "Wrong argument" 
+          else 
+            let rec f list i =
+              match list with
+              | Void -> Node(elem, Void)
+              | Node(a, next) -> if i = index then Node(elem, list) else Node(a, f next (i+1) )
+            thisList <- (f thisList 1)
+        
+      member this.RemoveHead() =
+        match thisList with
+        | Void -> ()
+        | Node(a, next) -> thisList <- next
+
+      member this.RemoveTail() =
+        let rec f list =
+          match list with
+          | Void -> Void
+          | Node(_, Void) -> Void
+          | Node(a, next) -> Node(a, f next)
+        thisList <- f thisList
+
+      member this.RemoveNode(elem) =
+        let rec f list =
+          match list with
+          | Void -> Void
+          | Node(a, next) -> if a = elem then next else Node(a, f next)
+        thisList <- f thisList
+
+      member this.TryFind(f1) =
+        let rec f list =
+          match list with
+          | Void -> None
+          | Node(a, next) ->
+            if (f1 a) then Some(a) else (f next)
+        f thisList
+
+      member this.Concat(anotherList) =
+        let rec f list =
+          match list with
+          | Void -> (anotherList :?> AbstractList<'A>).GetList()
+          | Node(a, next) -> Node(a, f next)
+        thisList <- f thisList
+  end
+
+
+type ArrayList<'A when 'A : equality>(L : 'A[]) =
+  class
+    let mutable thisList = L
+
+    member this.GetList() = thisList
+
+    member this.PrintList() = 
+      Array.iter (fun x -> printf "%A " x) thisList
+      printf "\n"
+
+    interface IList<'A> with
+
+      member this.InsertHead elem = thisList <- Array.append [|elem|] thisList 
+
+      member this.InsertTail elem = thisList <- Array.append thisList [|elem|]
+
+      member this.InsertAt elem index =           //if index > length of list then inserts element at tail
+        if index < 1 
+          then 
+            failwith "Wrong argument" 
+          else 
+            let length = Array.length thisList
+            if index = 1 then (this :> IList<'A>).InsertHead elem
+            elif index >= length then (this :> IList<'A>).InsertTail elem
+            else
+              let temp = Array.zeroCreate (length+1)
+              Array.blit thisList 0 temp 0 (index-1)
+              temp.[index-1] <- elem
+              Array.blit thisList (index-1) temp (index) (length-index+1)
+              thisList <- temp
+
+      member this.RemoveHead() =
+        match Array.length thisList with
+        | 0 -> ()
+        | 1 -> thisList <- [||] 
+        | n ->
+          let temp = Array.zeroCreate (n-1)
+          Array.blit thisList 1 temp 0 (n-1)
+          thisList <- temp
+
+      member this.RemoveTail() =
+        match Array.length thisList with
+        | 0 -> ()
+        | 1 -> thisList <- [||] 
+        | n ->
+          let temp = Array.zeroCreate (n-1)
+          Array.blit thisList 0 temp 0 (n-1)
+          thisList <- temp
+
+      member this.RemoveNode(elem) =
+        match Array.length thisList with
+        | 0 -> ()
+        | 1 -> if thisList.[0] = elem then thisList <- [||] else () 
+        | length ->
+          match Array.tryFindIndex ((=)elem) thisList with
+          | None -> ()
+          | Some i ->
+            if i = 0 then 
+              (this :> IList<'A>).RemoveHead()
+            elif i = (length-1) then 
+              (this :> IList<'A>).RemoveTail()
+            else 
+              let temp = Array.zeroCreate (length-1)
+              Array.blit thisList 0 temp 0 i
+              Array.blit thisList (i+1) temp i (length-i-1)
+              thisList <- temp
+    
+      member this.TryFind(f1) = Array.tryFind f1 thisList
+
+      member this.Concat(anotherList) = thisList <- Array.append thisList ( (anotherList :?> ArrayList<'A>).GetList() )
+  end
+
+[<EntryPoint>]
+let main args =
+  let list1 = Node(1, Node(3, Node(4, Node(5, Void) ) ) )
+  let abList = new AbstractList<int>(list1)
+  printf "Abstract list: " 
+  abList.PrintList()
+  printf "Insert 0 at head: "
+  (abList :> IList<int>).InsertHead 0
+  abList.PrintList()
+  printf "Insert 6 at tail: "
+  (abList :> IList<int>).InsertTail 6
+  abList.PrintList()
+  printf "Insert 2 at third node: "
+  (abList :> IList<int>).InsertAt 2 3
+  abList.PrintList()
+  printf "Remove node from head: "
+  (abList :> IList<int>).RemoveHead()
+  abList.PrintList()
+  printf "Remove node from tail: "
+  (abList :> IList<int>).RemoveTail()
+  abList.PrintList()
+  printf "Remove third node : "
+  (abList :> IList<int>).RemoveNode 3
+  abList.PrintList()
+  printf "Find node divisible by 5 : "
+  let x = (abList :> IList<int>).TryFind (fun x -> x % 5 = 0 )
+  match x with
+  | None   -> printf "Not found\n"
+  | Some x -> printf "%d\n" x
+  let list2 = Node(10, Node(11, Void) )
+  let anList = new AbstractList<int>(list2)
+  printf "Another abstract list: " 
+  anList.PrintList()
+  printf "Concat lists: "
+  (abList :> IList<int>).Concat anList
+  abList.PrintList()
+  printf "\n"
+
+  let array1 = [|1 ; 3 ; 4 ; 5|]
+  let arList = new ArrayList<int>(array1)
+  printf "Array list: " 
+  arList.PrintList()
+  printf "Insert 0 at head: "
+  (arList :> IList<int>).InsertHead 0
+  arList.PrintList()
+  printf "Insert 6 at tail: "
+  (arList :> IList<int>).InsertTail 6
+  arList.PrintList()
+  printf "Insert 2 at third node: "
+  (arList :> IList<int>).InsertAt 2 3
+  arList.PrintList()
+  printf "Remove node from head: "
+  (arList :> IList<int>).RemoveHead()
+  arList.PrintList()
+  printf "Remove node from tail: "
+  (arList :> IList<int>).RemoveTail()
+  arList.PrintList()
+  printf "Remove third node : "
+  (arList :> IList<int>).RemoveNode 3
+  arList.PrintList()
+  printf "Find node divisible by 5 : "
+  let x = (arList :> IList<int>).TryFind (fun x -> x % 5 = 0 )
+  match x with
+  | None   -> printf "Not found\n"
+  | Some x -> printf "%d\n" x
+  let array2 = [|10 ; 11|]
+  let arList2 = new ArrayList<int>(array2)
+  printf "Another abstract list: " 
+  arList2.PrintList()
+  printf "Concat lists: "
+  (arList :> IList<int>).Concat arList2
+  arList.PrintList()
+
+  0
