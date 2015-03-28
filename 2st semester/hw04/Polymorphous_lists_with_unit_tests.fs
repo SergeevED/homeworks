9deb98012462a81bc01c77c867dabe3dd8a80e1a
SergeevED <sergeev.ev.dm@gmail.com>
2015-03-28 23:11:21 +0300

    hw04
    hw04

---
+++ 2st semester/hw04/Polymorphous_lists_with_unit_tests.fs
@@ -0,0 +1,544 @@
+﻿module hw
+open NUnit.Framework
+
+type IList<'A when 'A : equality> =
+  interface
+    abstract Length     : unit -> int
+    abstract Pop        : unit -> Option<'A>
+    abstract PrintList  : unit -> unit
+    abstract InsertHead : 'A -> unit
+    abstract InsertTail : 'A -> unit
+    abstract InsertAt   : 'A -> int -> bool
+    abstract RemoveHead : unit -> unit
+    abstract RemoveTail : unit -> unit
+    abstract RemoveAt   : int -> bool
+    abstract TryFind    : ('A -> bool) -> Option<'A>
+    abstract Concat     : IList<'A> -> unit
+  end
+
+type List<'A> = Void | Node of 'A * List<'A>
+
+type ATDList<'A when 'A : equality>(L : List<'A>) =
+  class
+    let mutable thisList = L
+
+    interface IList<'A> with
+    
+      member this.Length() = 
+        let rec f list length =
+          match list with
+          | Void -> length
+          | Node(_, next) -> f next (length+1)
+        f thisList 0
+
+      member this.Pop() =
+        match thisList with
+        | Void -> None
+        | Node(a, _) -> 
+          (this :> IList<'A>).RemoveHead()
+          Some a
+
+      member this.PrintList() = 
+        let rec f list =
+          match list with
+          | Void -> ()
+          | Node(a, next) -> 
+            printf "%A " a
+            f next
+        f thisList
+        printf "\n"
+
+      member this.InsertHead elem = 
+        thisList <- Node(elem, thisList)
+
+      member this.InsertTail elem  =
+        let rec f list =
+          match list with
+          | Void -> Node(elem, Void)
+          | Node(a, next) -> Node(a, f next)
+        thisList <- f thisList
+
+      member this.InsertAt elem index = 
+        if (index < 1) || ( index > (this :> IList<'A>).Length() + 1 ) then false 
+          else 
+            let rec f list i =
+              match list with
+              | Void -> Node(elem, Void)
+              | Node(a, next) -> if i = index then Node(elem, list) else Node(a, f next (i+1) )
+            thisList <- (f thisList 1)
+            true
+        
+      member this.RemoveHead() =
+        match thisList with
+        | Void -> ()
+        | Node(a, next) -> thisList <- next
+
+      member this.RemoveTail() =
+        let rec f list =
+          match list with
+          | Void -> Void
+          | Node(_, Void) -> Void
+          | Node(a, next) -> Node(a, f next)
+        thisList <- f thisList
+
+      member this.RemoveAt index =
+        if (index < 1) || ( index > (this :> IList<'A>).Length() ) then false       
+          else 
+            let rec f list i =
+              match list, i with
+              | Void, _ -> Void
+              | Node(_, next), i when (i = index) -> next
+              | Node(a, next), i -> Node(a, f next (i+1))
+            thisList <- f thisList 1
+            true
+
+      member this.TryFind f1 =
+        let rec f list =
+          match list with
+          | Void -> None
+          | Node(a, next) ->
+            if (f1 a) then Some(a) else (f next)
+        f thisList
+
+      member this.Concat anotherList =
+        let rec f list = 
+          match list with   
+          | Node(x, next) -> Node(x, f next)
+          | Void ->
+            match anotherList.Pop() with
+            | None -> Void
+            | Some a -> Node(a, f list)
+        thisList <- f thisList
+           
+  end
+
+
+type ArrayList<'A when 'A : equality>(L : 'A[]) =
+  class
+    let mutable thisList = L
+
+    interface IList<'A> with
+
+      member this.Length() = Array.length thisList
+
+      member this.Pop() =
+        match thisList with
+        | [||] -> None
+        | _ -> 
+          let temp = thisList.[0]
+          (this :> IList<'A>).RemoveHead()
+          Some temp
+
+      member this.PrintList() = 
+        Array.iter (fun x -> printf "%A " x) thisList
+        printf "\n"
+
+      member this.InsertHead elem = thisList <- Array.append [|elem|] thisList 
+
+      member this.InsertTail elem = thisList <- Array.append thisList [|elem|]
+
+      member this.InsertAt elem index =
+        let length = Array.length thisList
+        if (index < 1) || (index > length + 1) then false          
+          else 
+            if index = 1 then 
+              (this :> IList<'A>).InsertHead elem
+              true
+            elif index = length + 1 then 
+              (this :> IList<'A>).InsertTail elem
+              true
+            else
+              let temp = Array.zeroCreate (length+1)
+              Array.blit thisList 0 temp 0 (index-1)
+              temp.[index-1] <- elem
+              Array.blit thisList (index-1) temp (index) (length-index+1)
+              thisList <- temp
+              true
+
+      member this.RemoveHead() =
+        match Array.length thisList with
+        | 0 -> ()
+        | 1 -> thisList <- [||] 
+        | n ->
+          let temp = Array.zeroCreate (n-1)
+          Array.blit thisList 1 temp 0 (n-1)
+          thisList <- temp
+
+      member this.RemoveTail() =
+        match Array.length thisList with
+        | 0 -> ()
+        | 1 -> thisList <- [||] 
+        | n ->
+          let temp = Array.zeroCreate (n-1)
+          Array.blit thisList 0 temp 0 (n-1)
+          thisList <- temp
+
+      member this.RemoveAt index =
+        let length = Array.length thisList
+        if (index < 1) || (index > length) then false
+        elif index = 1 then
+          (this :> IList<'A>).RemoveHead()
+          true
+        elif index = length then
+          (this :> IList<'A>).RemoveTail()
+          true
+        else
+          let temp = Array.zeroCreate (length-1)
+          Array.blit thisList 0 temp 0 (index-1)
+          Array.blit thisList index temp (index-1) (length-index)
+          thisList <- temp
+          true
+    
+      member this.TryFind f1 = Array.tryFind f1 thisList
+
+      member this.Concat anotherList = 
+        let rec f (anotherList : IList<'A>) =
+          match anotherList.Pop() with
+          | None -> ()
+          | Some a -> 
+            thisList <- Array.append thisList [|a|]
+            f anotherList
+        f anotherList
+  end
+
+
+let listToArr mList = 
+  let f() = 
+    match (mList :> IList<'A>).Pop() with
+    | None -> 
+      failwith "Wrong result in testing"
+    | Some x -> x
+  let length = mList.Length()
+  Array.map (fun x -> f() ) [|1..length|]
+
+
+[<Test>]
+let ArrayListPopEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<obj>(array) :> IList<obj>
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ArrayListInsertHeadEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  mList.InsertHead 1
+  Assert.AreEqual(Some 1, mList.Pop())
+
+[<TestCase([|1; 2; 3; 4; 5|], 100, Result = [|100; 1; 2; 3; 4; 5|])>]
+[<TestCase([|'a'; 'b'; 'c'|], 'x', Result = [|'x'; 'a'; 'b'; 'c'|])>]
+[<TestCase([| false; false; false |], true, Result = [| true; false; false; false |])>]
+let ArrayListInsertHead array x =
+  let mList = new ArrayList<'A>(array) :> IList<'A>
+  mList.InsertHead x
+  listToArr mList 
+
+[<Test>]
+let ArrayListInsertTailEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  mList.InsertTail 1
+  Assert.AreEqual([|1|], listToArr mList)
+
+[<TestCase([|1; 2; 3; 4; 5|], 100, Result = [|1; 2; 3; 4; 5; 100|])>]
+[<TestCase([|'a'; 'b'; 'c'|], 'x', Result = [|'a'; 'b'; 'c'; 'x'|])>]
+[<TestCase([| false; false; false |], true, Result = [| false; false; false; true|])>]
+let ArrayListInsertTail array x =
+  let mList = new ArrayList<'A>(array) :> IList<'A>
+  mList.InsertTail x
+  listToArr mList
+
+[<Test>]
+let ArrayListInsertAtEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  let b = mList.InsertAt 9 1
+  Assert.AreEqual( (Some 9, true) , (mList.Pop(), b) )
+
+[<TestCase([|1; 2; 3; 4; 5|], 100, -1, Result = false)>]
+[<TestCase([|1; 2; 3; 4; 5|], 100, 0, Result = false)>]
+[<TestCase([|'a'; 'b'; 'c'|], 'x', 5, Result = false)>]
+let ArrayListInsertAtWrongIndex array x index = 
+  let mList = new ArrayList<'A>(array) :> IList<'A>
+  mList.InsertAt x index 
+  
+[<TestCase([|1; 3|], 2, 2, Result = [|1; 2; 3|])>]
+[<TestCase([|1; 2|], 3, 3, Result = [|1; 2; 3|])>]
+[<TestCase([|2; 3|], 1, 1, Result = [|1; 2; 3|])>]
+[<TestCase([|'a'; 'c'|], 'b', 2, Result = [|'a'; 'b'; 'c'|])>]
+let ArrayListInsertAt array x index = 
+  let mList = new ArrayList<'A>(array) :> IList<'A>
+  let b = mList.InsertAt x index
+  listToArr mList 
+ 
+[<Test>]
+let ArrayListInsertLongList() = 
+  let array = [|
+     1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+     1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+     1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+     1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+  |]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  let b = mList.InsertAt 100 21
+  let ans = [|
+    1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;100
+    1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+    1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+    1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;
+  |]
+  Assert.AreEqual( ans , listToArr mList )
+
+[<Test>]
+let ArrayListRemoveHeadEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  mList.RemoveHead()
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ArrayListRemoveHeadList() = 
+  let mList = new ArrayList<int>([|1;2;3;4;5;6;7;8;9;0|]) :> IList<int>
+  mList.RemoveHead()
+  Assert.AreEqual([|2;3;4;5;6;7;8;9;0|], listToArr mList)
+
+[<Test>]
+let ArrayListRemoveTailEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<obj>(array) :> IList<obj>
+  mList.RemoveTail()
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ArrayListRemoveTailList() = 
+  let mList = new ArrayList<int>([|1;2;3;4;5;6;7;8;9;0|]) :> IList<int>
+  mList.RemoveTail()
+  Assert.AreEqual([|1;2;3;4;5;6;7;8;9|], listToArr mList)
+
+[<Test>]
+let ArrayListRemoveAtEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  let b = mList.RemoveAt 1
+  Assert.AreEqual(false, b )
+
+[<Test>]
+let ArrayListRemoveAtWrongIndex() = 
+  let mList = new ArrayList<int>([|1;2;3|]) :> IList<int>
+  let b = mList.RemoveAt 4 
+  Assert.AreEqual(false, b)
+
+[<Test>]
+let ArrayListRemoveAtList() = 
+  let mList = new ArrayList<char>([|'a';'b';'c';'d';'e';'f'|]) :> IList<char>
+  let b1 = mList.RemoveAt 1
+  let b2 = mList.RemoveAt 5
+  let b3 = mList.RemoveAt 3
+  Assert.AreEqual(true, b1)
+  Assert.AreEqual(true, b2)
+  Assert.AreEqual(true ,b3)
+  Assert.AreEqual([|'b';'c';'e'|] , listToArr mList)
+
+[<Test>]
+let ArrayListFindEmptyList() = 
+  let array = [||]
+  let mList = new ArrayList<obj>(array) :> IList<obj>
+  let ans = mList.TryFind (fun x -> true) 
+  Assert.AreEqual(None, ans)
+
+[<Test>]
+let ArrayListFindNothing() = 
+  let array = [|1; 2; 3; 4; 5|]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  let ans = mList.TryFind (fun x -> if x > 10 then true else false) 
+  Assert.AreEqual(None, ans)
+
+[<Test>]
+let ArrayListTryFind01() =
+  let array = [|1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20|]
+  let mList = new ArrayList<int>(array) :> IList<int>
+  let ans = mList.TryFind (fun x -> if (x % 2) = 0 then true else false)
+  Assert.AreEqual(Some 2, ans)
+
+[<Test>]
+let ArrayListConcatEmptyLists() =
+  let mList1 = new ArrayList<obj>([||]) :> IList<obj>
+  let mList2 = new ArrayList<obj>([||]) :> IList<obj>
+  mList1.Concat mList2
+  Assert.AreEqual(None, mList1.Pop())
+
+[<Test>]
+let ArrayListConcatLists() =
+  let mList1 = new ArrayList<char>([|'a';'b'|]) :> IList<char>
+  let mList2 = new ArrayList<char>([|'c';'d'|]) :> IList<char>
+  mList1.Concat mList2
+  Assert.AreEqual([|'a';'b';'c';'d'|], listToArr mList1)
+
+[<Test>]
+let ArrayConcatWithATDList() =
+  let mList1 = new ArrayList<char>( [|'a';'b'|] ) :> IList<char>
+  let mList2 = new ATDList<char>( Node('c', Node('d', Void)) ) :> IList<char>
+  mList1.Concat mList2
+  Assert.AreEqual([|'a';'b';'c';'d'|], listToArr mList1)
+
+[<Test>]
+let ATDListPopEmptyList() = 
+  let list = Void 
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ATDListInsertHeadEmptyList() = 
+  let list = Void
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  mList.InsertHead 1
+  Assert.AreEqual([|1|], listToArr mList)
+
+[<Test>]
+let ATDListInsertHead() =
+  let list = Node(1, Node(2, Node(3, Void)))
+  let mList = new ATDList<int>(list) :> IList<int>
+  mList.InsertHead 0
+  Assert.AreEqual([|0; 1; 2; 3|], listToArr mList)
+
+[<Test>]
+let ATDListInsertTailEmptyList() = 
+  let list = Void
+  let mList = new ATDList<int>(list) :> IList<int>
+  mList.InsertTail 1
+  Assert.AreEqual([|1|], listToArr mList)
+
+[<Test>]
+let ATDListInsertTail() =
+  let list = Node('a', Node('b', Node('c', Void)))
+  let mList = new ATDList<char>(list) :> IList<char>
+  mList.InsertTail 'z'
+  Assert.AreEqual([|'a';'b';'c';'z'|], listToArr mList)
+
+[<Test>]
+let ATDListInsertAtEmptyList() = 
+  let list = Void
+  let mList = new ATDList<int>(list) :> IList<int>
+  let b = mList.InsertAt 9 1
+  Assert.AreEqual( (Some 9, true) , (mList.Pop(), b) )
+
+[<Test>]
+let ATDListInsertAtWrongIndex() = 
+  let list = Node(1, Node(2, Node(3, Void)))
+  let mList = new ATDList<int>(list) :> IList<int>
+  let b = mList.InsertAt 5 -1
+  Assert.AreEqual( false, b )
+  
+[<Test>]
+let ATDListInsertAt() =
+  let list = Node('b', Node('c', Node('d', Void)))
+  let mList = new ATDList<char>(list) :> IList<char>
+  let b1 = mList.InsertAt 'a' 1
+  let b2 = mList.InsertAt 'e' 5
+  let b3 = mList.InsertAt 'z' 3
+  Assert.AreEqual( (true, true, true), (b1,b2,b3))
+  Assert.AreEqual([|'a';'b';'z';'c';'d';'e'|], listToArr mList)
+
+[<Test>]
+let ATDListRemoveHeadEmptyList() = 
+  let list = Void
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  mList.RemoveHead()
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ATDListRemoveHeadList() = 
+  let list = Node(1, Node(2, Node(3, Void))) 
+  let mList = new ATDList<int>(list) :> IList<int>
+  mList.RemoveHead()
+  Assert.AreEqual([|2;3|], listToArr mList)
+
+[<Test>]
+let ATDListRemoveTailEmptyList() = 
+  let list = Void
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  mList.RemoveTail()
+  Assert.AreEqual(None, mList.Pop())
+
+[<Test>]
+let ATDListRemoveTail() = 
+  let list = Node(1, Node(2, Node(3, Void)))
+  let mList = new ATDList<int>(list) :> IList<int>
+  mList.RemoveTail()
+  Assert.AreEqual([|1;2|], listToArr mList)
+
+[<Test>]
+let ATDListRemoveAtEmptyList() = 
+  let list = Void
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  let b = mList.RemoveAt 1
+  Assert.AreEqual(false, b)
+
+[<Test>]
+let ATDListRemoveAtWrongIndex() = 
+  let list = Node(1, Node(2, Node(3, Node(4, Void))))
+  let mList = new ATDList<int>(list) :> IList<int>
+  let b = mList.RemoveAt 5 
+  Assert.AreEqual(false, b)
+  Assert.AreEqual([|1;2;3;4|], listToArr mList)
+
+[<Test>]
+let ATDListRemoveAt() = 
+  let list = Node('a', Node('b', Node('c', Node('d', Node('e', Node('f', Void))))))
+  let mList = new ATDList<char>(list) :> IList<char>
+  let b1 = mList.RemoveAt 1
+  let b2 = mList.RemoveAt 5
+  let b3 = mList.RemoveAt 3
+  Assert.AreEqual(true, b1)
+  Assert.AreEqual(true, b2)
+  Assert.AreEqual(true ,b3)
+  Assert.AreEqual([|'b';'c';'e'|] , listToArr mList)
+
+[<Test>]
+let ATDListFindEmptyList() = 
+  let list = Void
+  let mList = new ATDList<obj>(list) :> IList<obj>
+  let ans = mList.TryFind (fun x -> true) 
+  Assert.AreEqual(None, ans)
+
+[<Test>]
+let ATDListFindNothing() = 
+  let list = Node(1, Node(2, Node(3, Node(4, Void))))
+  let mList = new ATDList<int>(list) :> IList<int>
+  let ans = mList.TryFind (fun x -> if x > 10 then true else false) 
+  Assert.AreEqual(None, ans)
+
+[<Test>]
+let ATDListTryFind01() =
+  let list = Node(1, Node(2, Node(3, Node(4, Void))))
+  let mList = new ATDList<int>(list) :> IList<int>
+  let ans = mList.TryFind (fun x -> if (x % 2) = 0 then true else false)
+  Assert.AreEqual(Some 2, ans)
+
+[<Test>]
+let ATDListConcatEmptyLists() =
+  let mList1 = new ATDList<obj>(Void) :> IList<obj>
+  let mList2 = new ATDList<obj>(Void) :> IList<obj>
+  mList1.Concat mList2
+  Assert.AreEqual(None, mList1.Pop())
+
+[<Test>]
+let ATDListConcatLists() =
+  let mList1 = new ATDList<char>( Node('a', Node('b', Void)) ) :> IList<char>
+  let mList2 = new ATDList<char>( Node('c', Node('d', Void)) ) :> IList<char>
+  mList1.Concat mList2
+  Assert.AreEqual([|'a';'b';'c';'d'|], listToArr mList1)
+
+[<Test>]
+let ATDConcatWithArrayList() =
+  let mList1 = new ATDList<char>( Node('a', Node('b', Void)) ) :> IList<char>
+  let mList2 = new ArrayList<char>( [|'c';'d'|] ) :> IList<char>
+  mList1.Concat mList2
+  Assert.AreEqual([|'a';'b';'c';'d'|], listToArr mList1)
+
+
+
+[<EntryPoint>]
+let main argv =
+
+  0
---
+++ 2st semester/hw04/Stack_calculator_with_unit_tests.fs
@@ -0,0 +1,290 @@
+﻿module hw
+open NUnit.Framework
+
+type Stack<'A>(V : list<'A>) =
+  class
+    let mutable list = V
+    member this.Size() = List.length list
+    member this.Reverse() = list <- (List.rev list)
+    member this.Top() = 
+      if this.Size() = 0 
+      then None
+      else Some (List.head list)
+    member this.Push x = list <- x :: list
+    member this.Pop() =
+      if this.Size() = 0 
+      then None
+      else
+        let head = this.Top()
+        list <- List.tail list
+        head
+  end
+
+
+let rec findVariableValue list (variable : string) =
+  match list with
+  | (v, value) :: tail when v = variable -> Some value
+  | (v, value) :: tail when not (v = variable) -> findVariableValue tail variable
+  | _ -> None
+
+
+let precedence c =
+  match c with
+  | '^' -> 3
+  | '*' | '/' | '%' -> 2
+  | '+' | '-' -> 1
+  | _ -> 0
+
+let isLeftAssoc c =
+  match c with
+  | '+' | '-' | '*' | '/' | '%' -> true
+  | '^' -> false
+  | _ -> failwith "not an operator"
+
+let isOperator c = 
+  match c with
+  | '+' | '-' | '*' | '/' | '%'| '^' -> true
+  | _ -> false
+
+let isNumber (c : string) =
+  match ( String.length c ) with
+  | 0 -> false
+  | 1 -> if (System.Char.IsDigit (c.Chars(0))) then true else false
+  | x -> if (System.Char.IsDigit (c.Chars(1))) then true else false 
+
+let rec scanToken (stack : Stack<char>) =
+  match stack.Top() with
+  | None -> (None, true)
+  | Some x ->
+    match x with
+    | x when (isOperator x) -> 
+      let temp = stack.Pop()
+      if (x = '-') && (not (stack.Size() = 0)) && (System.Char.IsDigit (Option.get (stack.Top() )))
+      then
+        match (scanToken stack) with 
+        | (Some strNumber, true) -> (Some("-" + strNumber), true)
+        | _ -> failwith "ScanToken() wrong output"
+      else
+        (Some (System.Char.ToString (Option.get (temp ))), true)  
+    | ' ' -> 
+      ignore( stack.Pop() )
+      scanToken stack
+    | x when (System.Char.IsDigit x) ->
+      let rec scanNumber strNumber =
+        match stack.Top() with
+        | None -> strNumber
+        | x when (System.Char.IsDigit (Option.get x)) ->
+          let temp = System.Char.ToString (Option.get ( stack.Pop() ))
+          strNumber + (scanNumber temp)
+        | _ -> strNumber
+      let x = Option.get ( stack.Pop() )
+      (Some (scanNumber (System.Char.ToString x)), true)
+    | x when (System.Char.IsLetter x) ->
+      let rec scanLetter strNumber =
+        match stack.Top() with
+        | None -> strNumber
+        | x when (System.Char.IsLetter (Option.get x)) ->
+          let temp = System.Char.ToString (Option.get ( stack.Pop() ))
+          strNumber + (scanLetter temp)
+        | _ -> strNumber
+      let x = Option.get ( stack.Pop() )
+      (Some (scanLetter (System.Char.ToString x)), true)
+    | x when (x = '(' || x = ')') -> (Some (System.Char.ToString (Option.get ( stack.Pop() ))) , true)
+    | _ -> (None, false) 
+    
+let calculate (stack : Stack<string>) =
+  let result = Stack<int>([])
+  let mutable isCorrect = true
+  while (stack.Size() > 0) && (isCorrect) do
+    let token = stack.Pop()
+    match token with
+    | Some(value) when (isNumber (value)) -> result.Push (int32 (value))
+    | Some(operator) when (isOperator (operator.Chars(0)) ) && ((String.length operator = 1)) ->
+      if result.Size() < 2 
+        then isCorrect <- false
+        else
+          let operand2 = result.Pop()
+          let operand1 = result.Pop()
+          match operator with
+          | "+" -> result.Push ((int32(Option.get(operand1))) + (int32(Option.get(operand2)))) 
+          | "-" -> result.Push ((int32(Option.get(operand1))) - (int32(Option.get(operand2))))
+          | "*" -> result.Push ((int32(Option.get(operand1))) * (int32(Option.get(operand2))))
+          | "%" -> 
+            if (operand2 = Some 0)
+              then isCorrect <- false
+              else result.Push ((int32(Option.get(operand1))) % (int32(Option.get(operand2))))
+          | "/" ->
+            if (operand2 = Some 0)
+              then isCorrect <- false
+              else result.Push ((int32(Option.get(operand1))) / (int32(Option.get(operand2))))
+          | "^" ->
+            match operand1, operand2 with
+            | _ , Some x when x < 0 -> isCorrect <- false
+            | Some 0 , Some 0 -> isCorrect <- false
+            | Some val1 , Some val2 when val1 = 0 -> result.Push 0
+            | Some x , Some 0 -> result.Push 1
+            | Some val1 , Some val2 -> result.Push (pown val1 val2)
+            | _ , _ -> isCorrect <- false
+          | _ -> isCorrect <- false
+    | _ -> isCorrect <- false      
+  if isCorrect = false 
+    then None 
+    else 
+      if not (stack.Size() = 0) 
+      then None
+      else ( result.Pop() )
+
+type Program = Continue | EndCorrect | EndIncorrect 
+
+let stackCalc (str : string) variableList = 
+  let list = Array.toList ( str.ToCharArray() )
+  let mutable program = Continue
+  let mutable operatorWasScanned = true          
+  let outputStack = Stack<string>([])
+  let inputStack = Stack<char>(list)
+  let tempStack = Stack<char>([])
+  while program = Continue do
+    let mutable token = scanToken inputStack
+    match token with
+    | (_, false) -> program <- EndIncorrect
+    | (Some x, true) when (isNumber x) ->
+      if not operatorWasScanned 
+        then program <- EndIncorrect
+        else 
+          operatorWasScanned <- false
+          outputStack.Push x
+    | (Some x, true) when (System.Char.IsLetter (x.Chars(0)) ) -> 
+      if not operatorWasScanned 
+        then program <- EndIncorrect
+        else
+          operatorWasScanned <- false
+          match (findVariableValue variableList x) with
+          | None -> program <- EndIncorrect
+          | Some x -> outputStack.Push (string x)
+    | (Some x, true) when (isOperator (x.Chars(0)) ) && ((String.length x) = 1) ->     
+      if operatorWasScanned 
+        then program <- EndIncorrect
+        else
+          operatorWasScanned <- true
+          while (not (tempStack.Size() = 0)) &&
+          (isLeftAssoc (x.Chars(0)) && (precedence (x.Chars(0)) <= precedence (Option.get (tempStack.Top() )))
+          || not (isLeftAssoc (x.Chars(0) )) && (precedence (x.Chars(0)) < precedence (Option.get (tempStack.Top() )))) do
+            outputStack.Push (string (Option.get (tempStack.Pop() )))
+          tempStack.Push (x.Chars(0))
+    | (Some x, true) when x.Chars(0) = '(' -> 
+      if not operatorWasScanned
+        then program <- EndIncorrect
+        else tempStack.Push (x.Chars(0))
+    | (Some x, true) when x.Chars(0) = ')' ->
+      if operatorWasScanned
+        then program <- EndIncorrect
+        else
+          while (not (tempStack.Size() = 0)) && (not (tempStack.Top() = Some '('))  do
+            outputStack.Push (string (Option.get (tempStack.Pop() )))
+          if tempStack.Size() = 0 
+            then program <- EndIncorrect
+            else ignore (tempStack.Pop() )
+    | (None, true) -> program <- EndCorrect
+    | _ -> program <- EndIncorrect
+  match program with
+  | EndIncorrect -> None
+  | _ ->
+    while not (tempStack.Size() = 0) do 
+      outputStack.Push (string (Option.get (tempStack.Pop() )))
+    outputStack.Reverse() 
+    calculate outputStack
+
+
+[<Test>]
+let DivisionByZero() =
+  let str1 = "1/0"
+  let str2 = "1%0"
+  let values = [("x",0)]
+  Assert.AreEqual(None, stackCalc str1 [])
+  Assert.AreEqual(None, stackCalc str2 [])
+  Assert.AreEqual(None, stackCalc str1 values)
+
+[<Test>]
+let IncorrectPow() =
+  let str1 = "0^0"
+  let str2 = "1^(-1)"
+  let str3 = "1+2^"
+  let str4 = "2^x"
+  let values = [("x",-1)]
+  Assert.AreEqual(None, stackCalc str1 [])
+  Assert.AreEqual(None, stackCalc str2 [])
+  Assert.AreEqual(None, stackCalc str3 [])
+  Assert.AreEqual(None, stackCalc str4 values)
+
+[<Test>]
+let IncorrectOrder() =
+  let str1 = "1-"
+  let str2 = "*1-3"
+  let str3 = "5*/3"
+  let str4 = "1 2 + 3"
+  let str5 = "1+(1-3)"
+  let str6 = "1-2+3x%5"
+  let values = [("x",-1)]
+  Assert.AreEqual(None, stackCalc str1 [])
+  Assert.AreEqual(None, stackCalc str2 [])
+  Assert.AreEqual(None, stackCalc str3 [])
+  Assert.AreEqual(None, stackCalc str4 [])
+  Assert.AreEqual(None, stackCalc str5 [])
+  Assert.AreEqual(None, stackCalc str6 values)
+
+[<Test>]
+let IncorrectSymbols() =
+  let str1 = "123*2$"
+  let str2 = "12+!2"
+  Assert.AreEqual(None, stackCalc str1 [])
+  Assert.AreEqual(None, stackCalc str2 [])
+
+[<Test>]
+let TestBracketsIncorrect() =
+  let str1 = "1*(2-)*2"
+  let str2 = "1*(2-3)2+1"
+  let str3 = "1*(2-3)x+1"
+  let str4 = "1+2*(1"
+  let str5 = "1*2+3)*2"
+  let values = [("x",-1)]
+  Assert.AreEqual(None, stackCalc str1 [])
+  Assert.AreEqual(None, stackCalc str2 [])
+  Assert.AreEqual(None, stackCalc str3 values)
+  Assert.AreEqual(None, stackCalc str4 [])
+  Assert.AreEqual(None, stackCalc str5 [])
+
+[<Test>]
+let TestValues() =
+  let str1 = "a+b-c*d/e%f"
+  let values1 = [("a",10);("b",8);("c",9);("d",10);("e",2);("f",4)]
+  let str2 = "aaaaaaaaaaaaaaa * bbbbbbbbbbbbbbb"
+  let values2 = [("aaaaaaaaaaaaaaa",10);("bbbbbbbbbbbbbbb",8)]
+  Assert.AreEqual(Some 17, stackCalc str1 values1)
+  Assert.AreEqual(Some 80, stackCalc str2 values2)
+
+[<Test>]
+let TestUnaryMinus() =
+  let str1 = "(-5)+(-6)*((-2)*3)"
+  Assert.AreEqual(Some 31, stackCalc str1 [])
+
+[<Test>]
+let Test01() =
+  let str1 = "100 + 200 - 400 * 30 / 50 % 39"
+  Assert.AreEqual(Some 294, stackCalc str1 [])
+
+[<Test>]
+let Test02() =
+  let str1 = "(1234567 *123 - 98765432+1)/12345+555555"
+  let values1 = [("x",12345)]
+  Assert.AreEqual(Some 559855, stackCalc str1 values1)
+
+[<Test>]
+let Test03() =
+  let str1 = "1+2*4^3^2"
+  Assert.AreEqual(Some 524289, stackCalc str1 [])
+
+
+[<EntryPoint>]
+let main args =  
+  0
+  
