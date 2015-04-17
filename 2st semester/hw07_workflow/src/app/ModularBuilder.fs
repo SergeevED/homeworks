9c4a46c8d095954107e8659f4eb9469ad8f18480
SergeevED <sergeev.ev.dm@gmail.com>
2015-04-17 23:24:48 +0300

    fixed_bug_in_modular_arithmetics
    now ModularBuilder returns correct residue

--- 2st semester/hw07_workflow/src/app/ModularBuilder.fs
+++ 2st semester/hw07_workflow/src/app/ModularBuilder.fs
@@ -6,15 +6,20 @@ let divBy a b =
 let modBy a b = 
   if a = 0 then None else Some(b % a)
 
+let getResidue x n =                          //returns residue of the integer x modulo n
+  match (x % n) with
+  | x when x < 0  -> x + n
+  | x -> x
+
 type ModularBuilder (n : int) =
-  member this.Return x = Some (x % n)
+  member this.Return x = Some(getResidue x n)
   member this.ReturnFrom x = 
     match x with
     | None -> None
-    | Some x -> Some (x % n)
+    | Some x -> Some(getResidue x n)
   member this.Bind(m , f) = 
     match m with
     | None -> None
-    | Some x -> f (x % n) 
+    | Some x -> f (getResidue x n) 
   member this.Bind(x , f) =
-    f (x % n)
+    f (getResidue x n)
