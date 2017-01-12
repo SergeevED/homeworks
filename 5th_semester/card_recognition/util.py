cfb38fcb2dd40ddfad3b6482797cfaf39813ff12
Evgeny Sergeev <sergeev.ev.dm@gmail.com>
2017-01-12 23:10:18 +0300

    change preprocessing

--- 5th_semester/card_recognition/util.py
+++ 5th_semester/card_recognition/util.py
@@ -1,11 +1,31 @@
 import cv2
 
 
-def preprocess(image):
+def preprocess(image, debug=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     denoised = cv2.fastNlMeansDenoising(gray, h=50, templateWindowSize=9, searchWindowSize=21)
     blur = cv2.GaussianBlur(denoised, (5, 5), 2)
     thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 9, 0)
-    denoised_thresh = cv2.fastNlMeansDenoising(thresh, h=40, templateWindowSize=9, searchWindowSize=21)
-    blur_thresh = cv2.GaussianBlur(denoised_thresh, (5, 5), 4)
-    return blur_thresh
+    thresh = (255 - thresh)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+    eroded = cv2.erode(thresh, kernel)
+    dilated = cv2.dilate(eroded, kernel)
+
+    dilated = (255 - dilated)
+
+    if debug:
+        cv2.imwrite("debug_proc_im/00_origin.jpg", image)
+        cv2.imwrite("debug_proc_im/10_denoised.jpg", denoised)
+        cv2.imwrite("debug_proc_im/20_blur.jpg", blur)
+        cv2.imwrite("debug_proc_im/40_thresh.jpg", thresh)
+
+        cv2.imwrite("debug_proc_im/45_eroded.jpg",eroded)
+        cv2.imwrite("debug_proc_im/46_dilated.jpg", dilated)
+
+    return dilated
+
+
+#image = cv2.imread("data_gen/cards/Qd.jpg")
+image = cv2.imread("out/card_01.jpg")
+preprocess(image, True)
