99d9ef75dac91efb6c801dbbe044c31db998280c
Evgeny Sergeev <sergeev.ev.dm@gmail.com>
2017-01-12 06:17:46 +0300

    rotate cards to right orientation

--- 5th_semester/card_recognition/main.py
+++ 5th_semester/card_recognition/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import random
 import os
+import copy
 
 from operator import itemgetter
 
@@ -15,19 +16,19 @@ def show_image(image_name, image):
 
 
 path_to_images = "images"
-if not os.path.exists(path_to_images ):
-    os.makedirs(path_to_images )
+if not os.path.exists(path_to_images):
+    os.makedirs(path_to_images)
 
 path_to_samples = "classifier_samples/"
 sample_width = 250
 sample_height = 350
 
-filename = "t5.jpg"
+filename = "t3.jpg"
 
 origin_image = cv2.imread(filename)
 
 gray = cv2.cvtColor(origin_image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite(path_to_images  + '/05_gray.jpg', gray)
+cv2.imwrite(path_to_images + '/05_gray.jpg', gray)
 
 blur = gray
 
@@ -36,11 +37,10 @@ cv2.imwrite(path_to_images + '/20_thresh.jpg', thresh)
 
 blur_thresh = thresh
 
-
-_, contours, _ = cv2.findContours(blur_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+_, contours, _ = cv2.findContours(blur_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 cont_im = np.ones((len(origin_image), len(origin_image[0]), 3)) * 255
-cv2.drawContours(cont_im, contours, -1, (0,0,0), 3)
+cv2.drawContours(cont_im, contours, -1, (0, 0, 0), 3)
 cv2.imwrite(path_to_images + '/40_contours.jpg', cont_im)
 
 filtered_contours = []
@@ -55,26 +55,48 @@ for cnt in contours:
             filtered_contours.append(approx)
 
 # rotate and scale cards
+cards = []
 for i in range(len(filtered_contours)):
+    flag = False
+    for j in range(i):
+        for k in range(4):
+            if cv2.pointPolygonTest(filtered_contours[j], tuple(filtered_contours[i][k][0]), False) > 0 or \
+                    cv2.pointPolygonTest(filtered_contours[i], tuple(filtered_contours[j][k][0]), False) > 0:
+                flag = True
+                break
+        if flag:
+            break
+
+    if flag:
+        continue
+
+    # if first and second vertices are a small side take vertices starting from next vertex
+    if (filtered_contours[i][0][0][0] - filtered_contours[i][1][0][0]) ** 2 + \
+                    (filtered_contours[i][0][0][1] - filtered_contours[i][1][0][1]) ** 2 > \
+                            (filtered_contours[i][1][0][0] - filtered_contours[i][2][0][0]) ** 2 + \
+                            (filtered_contours[i][1][0][1] - filtered_contours[i][2][0][1]) ** 2:
+        temp = copy.deepcopy(filtered_contours[i][0])
+        filtered_contours[i][0] = filtered_contours[i][1]
+        filtered_contours[i][1] = filtered_contours[i][2]
+        filtered_contours[i][2] = filtered_contours[i][3]
+        filtered_contours[i][3] = temp
+
     if cv2.contourArea(filtered_contours[i], True) < 0:
         filtered_contours[i] = filtered_contours[i][::-1]
+
     scaled_length = 250
     scaled_width = 350
     contour = np.array([[float(arr[0][0]), float(arr[0][1])] for arr in filtered_contours[i]], np.float32)
-    h = np.array([ [0,0],[scaled_length,0],[scaled_length,scaled_width],[0,scaled_width] ], np.float32)
+    h = np.array([[0, 0], [scaled_length, 0], [scaled_length, scaled_width], [0, scaled_width]], np.float32)
     transform = cv2.getPerspectiveTransform(contour, h)
     warp = cv2.warpPerspective(origin_image, transform, (scaled_length, scaled_width))
+    cards.append(warp)
     cv2.imwrite(path_to_images + '/' + str(i) + '_card.jpg', warp)
-
-# show stats for biggest contours
-#orig_image_area = len(origin_image) * len(origin_image[0])
-#for cnt in filtered_contours:
-#    print("{:.4f}".format(100.0 * cv2.contourArea(cnt) / orig_image_area))
 
 cont_im = np.ones((len(origin_image), len(origin_image[0]), 3)) * 255
 for cnt in filtered_contours:
-    cv2.drawContours(cont_im, [cnt], -1, (random.randint(40, 255),random.randint(0, 255),random.randint(0, 255)), 3)
-cv2.imwrite(path_to_images  + '/50_filtered_contours.jpg', cont_im)
+    cv2.drawContours(cont_im, [cnt], -1, (random.randint(40, 255), random.randint(0, 255), random.randint(0, 255)), 3)
+cv2.imwrite(path_to_images + '/50_filtered_contours.jpg', cont_im)
 
 
 def load_samples():
@@ -88,6 +110,7 @@ def load_samples():
             out_samples.append((card_name, image))
     return out_samples
 
+
 samples = load_samples()
 
 
@@ -99,8 +122,8 @@ def img_diff(img1, img2):
 
 
 def find_closest_card(raw_img, verbose=False):
-    assert(len(raw_img) == sample_height)
-    assert(len(raw_img[0]) == sample_width)
+    assert (len(raw_img) == sample_height)
+    assert (len(raw_img[0]) == sample_width)
     img = preprocess(raw_img)
     diff_results = [(sample[0], img_diff(img, sample[1])) for sample in samples]
     sorted_diff_results = sorted(diff_results, key=itemgetter(1))
