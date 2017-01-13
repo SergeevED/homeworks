64f3f539147c9b729cd9dab5a6a80928ffd07c16
Evgeny Sergeev <sergeev.ev.dm@gmail.com>
2017-01-13 20:13:33 +0300

    add rotating for images then long side of card is shorter then shord side

--- 5th_semester/card_recognition/util.py
+++ 5th_semester/card_recognition/util.py
@@ -3,10 +3,28 @@ import numpy as np
 import copy
 
 
+sample_width = 400
+sample_height = 560
+
+
 def preprocess(image, debug=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, np.average(gray) * 0.8, 255, cv2.THRESH_BINARY)
+
+    # if upper-right corner is darker than upper-left corner rotate image on 90 degrees
+    corner_width = 200
+    corner_height = 112
+    upper_left_corner = thresh[0:corner_height, 0:corner_width]
+    upper_right_corner = thresh[0:corner_height, \
+                         sample_width - corner_width:sample_width]
+    if (np.average(upper_left_corner) > np.average(upper_right_corner)):
+        h = np.array([[0, sample_height], [sample_width, sample_height], [sample_width, 0], [0, 0]], np.float32)
+        contour = np.array([(0, 0), (0, sample_height), (sample_width, sample_height), (sample_width, 0)], np.float32)
+        transform = cv2.getPerspectiveTransform(contour, h)
+        thresh = cv2.warpPerspective(thresh, transform, (sample_width, sample_height))
+
     thresh_not_corrupted = copy.deepcopy(thresh)
+
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     origin_image_area = len(image) * len(image[0])
@@ -29,14 +47,14 @@ def preprocess(image, debug=False):
     cv2.rectangle(cont_im, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cut_im = thresh_not_corrupted[y:y + h, x:x + w]
-    cut_im = cv2.resize(cut_im, (400, 560))
-
+    cut_im = cv2.resize(cut_im, (sample_width, sample_height))
 
     if debug:
         cv2.imwrite("debug_proc_im/00_origin.jpg", image)
         cv2.imwrite("debug_proc_im/10_gray.jpg", gray)
         cv2.imwrite("debug_proc_im/20_thresh.jpg", thresh_not_corrupted)
-        cv2.imwrite("debug_proc_im/30_contours.jpg", cont_im)
+        cv2.imwrite("debug_proc_im/25_upper_right.jpg", upper_right_corner)
+        cv2.imwrite("debug_proc_im/30_upper_left.jpg", upper_left_corner)
         cv2.imwrite("debug_proc_im/40_cut_im.jpg", cut_im)
 
     return cut_im
